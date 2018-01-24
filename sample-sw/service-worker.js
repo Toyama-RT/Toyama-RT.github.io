f303dc649bd541f7e01ad39a6042c07a28d0745f
Toyama-RT <ohshima@toyama-reha.or.jp>
2018-01-24 15:29:48 +0900

    Initial commit

---
+++ sample-sw/service-worker.js
@@ -0,0 +1,23 @@
+importScripts('serviceworker-cache-polyfill.js');
+
+// インストール時 (register時) に静的ファイルをキャッシュしておく
+self.oninstall = function(event) {
+  // .waitUntil() に渡された Promises が resolve されたら
+  // インストール完了
+  event.waitUntil(
+    caches.open('statics-v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/page.js',
+        '/offline_icon.png',
+        '/main.css']);
+    })
+  );
+};
+
+
+// ページヘのネットワークリクエストが来たらキャッシュにある
+// データを返す
+self.onfetch = function(event) {
+  event.respondWith(caches.match(event.request));
+};
