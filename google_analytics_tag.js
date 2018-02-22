8cd218a0044100c4c021506019c13a480bff7a8d
Toyama-RT <ohshima@toyama-reha.or.jp>
2018-02-22 11:09:44 +0900

    Initial commit

---
+++ google_analytics_tag.js
@@ -0,0 +1,9 @@
+//<!-- Global site tag (gtag.js) - Google Analytics -->
+  async src="https://www.googletagmanager.com/gtag/js?id=UA-82832782-3"
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-82832782-3');
+
