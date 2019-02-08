950fa24c709284a5f2e2f4845e5f678980215f4f
Toyama-RT <ohshima@toyama-reha.or.jp>
2019-02-08 13:37:44 +0900

    Initial commit

--- enchant/p-44fsw1/sw.js
+++ enchant/p-44fsw1/sw.js
@@ -146,7 +146,8 @@ self.addEventListener('install', function(event) {
     caches.open(CACHE_NAME)
       .then(function(cache) {
         console.log('Opened cache');
-        return cache.addAll(urlsToCache.map(url => new Request(url, {credentials: 'same-origin'})));
+        return cache.addAll(urlsToCache.map(url => new Request(url, {credentials: 'same-origin'})))
+      .then(() => self.skipWaiting());
       })
   );
 });
