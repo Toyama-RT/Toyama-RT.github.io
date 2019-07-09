0f09e1db3aa3f7ac6bf2ee62713471c6c9354f5a
ohshima_2TOPwin10 <ohshimajunichi@gmail.com>
2019-07-09 15:37:24 +0900

    Initial commit

--- web-mojiban/hcja63tb3/test.js
+++ web-mojiban/hcja63tb3/test.js
@@ -82,7 +82,7 @@ var path = `https://toyama-rt.github.io/web-mojiban/hcja63tb1`;
       new Audio('./snd/kesu.mp3').play();
       text1.value = text1.value.substring(0, text1.value.length -1 );
       setCursorend();
-     }, false);*/
+     }, false);
 
    // 数字
      var obj = document.getElementById('1');
@@ -91,7 +91,7 @@ var path = `https://toyama-rt.github.io/web-mojiban/hcja63tb1`;
       text1.value = text1.value + "１";
       setCursorend();
      }, false);
-/*     var obj = document.getElementById('2');
+     var obj = document.getElementById('2');
 	 obj.addEventListener("click", function() {
       new Audio('./snd/2.mp3').play();
       text1.value = text1.value + "２";
@@ -114,14 +114,14 @@ var path = `https://toyama-rt.github.io/web-mojiban/hcja63tb1`;
       new Audio('./snd/5.mp3').play();
       text1.value = text1.value + "５";
       setCursorend();
-     }, false);*/
+     }, false);
      var obj = document.getElementById('6');
 	 obj.addEventListener("click", function() {
       new Audio('./snd/6.mp3').play();
       text1.value = text1.value + "６";
       setCursorend();
      }, false);
-/*     var obj = document.getElementById('7');
+     var obj = document.getElementById('7');
 	 obj.addEventListener("click", function() {
       new Audio('./snd/7.mp3').play();
       text1.value = text1.value + "７";
