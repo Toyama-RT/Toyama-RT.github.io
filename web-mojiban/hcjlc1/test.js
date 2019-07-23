9afd988d111cb1cdf4dbbf74628c6edf58031475
ohshima_2TOPwin10 <ohshimajunichi@gmail.com>
2019-07-23 17:59:36 +0900

    Initial commit

--- web-mojiban/hcjlc1/test.js
+++ web-mojiban/hcjlc1/test.js
@@ -202,7 +202,7 @@ document.addEventListener("DOMContentLoaded", function(){ // htmlを読み込み
                synthes.voiceURI = 'Google 日本人';//'native';
                synthes.volume = 1;
                synthes.rate = 0.9;// ややゆっくり　一文字の発話はっきりする
-               synthes.pitch = 1;
+               synthes.pitch = 1.2;
                synthes.lang = 'ja-JP';
                synthes.text = '準備出来ました';
                //synthes.lang = 'en-US';
