dd0a7f72e15ef74e2b79313e6698dfedb2f8a7c3
ohshima_2TOPwin10 <ohshimajunichi@gmail.com>
2020-03-13 10:39:58 +0900

    Initial commit

--- web-mojiban/hcjlc2wwa1/sw.js
+++ web-mojiban/hcjlc2wwa1/sw.js
@@ -17,7 +17,7 @@
 
 //'use strict';
 const prefix = 'hcjlc2wwa1';
-const CACHE_NAME = prefix + '00';
+const CACHE_NAME = prefix + '01';
 
 var path = 'https://toyama-rt.github.io/web-mojiban/hcjlc2wwa1';
 
