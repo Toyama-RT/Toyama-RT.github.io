574b538585ed243e1c8245ed61ba8cb836607331
ohshima_2TOPwin10 <ohshimajunichi@gmail.com>
2020-06-04 10:51:36 +0900

    Initial commit

--- web-toy/g-01b041/sw.js
+++ web-toy/g-01b041/sw.js
@@ -1,19 +1,19 @@
-//*
-// *  Copyright 2015 Google Inc. All rights reserved.
-// *
-// *  Licensed under the Apache License, Version 2.0 (the "License");
-// *  you may not use this file except in compliance with the License.
-// *  You may obtain a copy of the License at
-// *
-// *      https://www.apache.org/licenses/LICENSE-2.0
-// *
-// *  Unless required by applicable law or agreed to in writing, software
-// *  distributed under the License is distributed on an "AS IS" BASIS,
-// *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// *  See the License for the specific language governing permissions and
-// *  limitations under the License
-// *
-// */
+/*
+ *  Copyright 2015 Google Inc. All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ *
+ */
 
 //'use strict';
 const prefix = 'g-01b041';
@@ -36,7 +36,7 @@ self.addEventListener('install', e => {
         path + `/js/lib/plugins/nineleap.enchant.js`,
         path + `/js/lib/plugins/ui.enchant.js`,
 
-        path + `/snd/jump.wav`,
+        path + `/snd/jump.mp3`,
 
         path + `/ico/webtoy.png`,
 
