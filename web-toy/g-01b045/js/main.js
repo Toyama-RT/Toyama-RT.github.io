152d07a888c9e568a4a6643f3c9bcc4801195f32
ohshima_2TOPwin10 <ohshimajunichi@gmail.com>
2021-01-22 15:38:53 +0900

    Initial commit

--- web-toy/g-01b045/js/main.js
+++ web-toy/g-01b045/js/main.js
@@ -84,6 +84,41 @@ window.onload = function() {
                   addBox();
                   addBox();
 
+                //四角形の作成2
+            var addBox2 = function(){
+                var box = new PhyBoxSprite(32, 32, enchant.box2d.DYNAMIC_SPRITE, 1.0, 0.5, 0.3, true);
+                box.backgroundColor = "blue";
+                box.position = {
+                        x: 130,
+                        y: 50
+                }
+                game.rootScene.addChild(box);
+                        box.ontouchstart = function() {
+
+    //タッチしたときの処理
+    box.addEventListener("touchstart", function (e) {
+		this.setAwake(false);		//物理演算をストップ
+    });
+    
+    //ドラッグ中の処理
+     box.addEventListener("touchmove", function (e) {
+        this.position = {x:e.x,y:e.y};		//現在のマウス座標をスプライトに通知
+    });
+    
+    //タッチを離したときの処理 
+    box.addEventListener("touchend", function (e) {
+        this.setAwake(true);				//物理演算を開始
+    });
+
+                        }
+            }
+            addBox2();
+            addBox2();
+            addBox2();
+            addBox2();
+            //addBox2();
+
+
                 //物理エンジン処理
                 game.rootScene.onenterframe = function() {
                         world.step(game.fps);
